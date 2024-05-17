5d39e9cd6c0528a05b51cbb52aa0a38aeb9963dd
c-node <chuckhead7@gmail.com>
2024-05-17 02:46:02 -0400

    save proof

--- script/src/main.rs
+++ script/src/main.rs
@@ -2,6 +2,7 @@
 use serde_json;
 use celestia_types::ExtendedHeader;
 use sp1_sdk::{ProverClient, SP1Stdin};
+use std::time::Instant;
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
@@ -21,10 +22,19 @@ fn main() {
     stdin.write_vec(encoded_2);
 
     let client = ProverClient::new();
-    let (pk, vk) = client.setup(&ELF);
+    let (pk, _vk) = client.setup(&ELF);
 
-    let proof = client.prove_compressed(&pk, stdin).expect("couldn't prove");
+
+    let start_time = Instant::now();
+    let mut proof = client.prove_compressed(&pk, stdin).expect("couldn't prove");
+    let end_time = Instant::now();
+    println!("Proving time: {:?}", end_time - start_time);
     //let mut public_values = client.execute(&ELF, stdin).unwrap();
-    let result = proof.public_values.to_vec();
+    //let result = proof.public_values.to_vec();
+    let result: bool = proof.public_values.read();
+    let proof_file = std::fs::File::create("proof.json").unwrap();
+    let pk_file = std::fs::File::create("pk.json").unwrap();
+    serde_json::to_writer(proof_file, &proof).unwrap();
+
     println!("result is: {:?}", result);
 }
