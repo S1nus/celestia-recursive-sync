a69564b51c26c26e7daab6765fa83211e89712ca
c-node <chuckhead7@gmail.com>
2024-05-23 20:42:32 -0400

    almost got it

--- script/src/main.rs
+++ script/src/main.rs
@@ -1,40 +1,54 @@
 //! A simple script to generate and verify the proof of a given program.
 use serde_json;
 use celestia_types::ExtendedHeader;
-use sp1_sdk::{ProverClient, SP1Stdin};
-use std::time::Instant;
+use sp1_sdk::{HashableKey, ProverClient, SP1Stdin, SP1CompressedProof};
+use std::{io::Write, time::Instant};
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
 fn main() {
-    // Generate proof.
+
+    // Initialize ProverClient
+    let client = ProverClient::new();
+    let (_pk, vk) = client.setup(ELF);
     let mut stdin = SP1Stdin::new();
-    let header1_file = std::fs::File::open("zkgenesis2.json").unwrap();
-    let header2_file = std::fs::File::open("nethead2.json").unwrap();
-    let h1: ExtendedHeader = serde_json::from_reader(header1_file).unwrap();
+
+    let proof_file = std::fs::File::open("proof.json").unwrap();
+    let proof: SP1CompressedProof = serde_json::from_reader(proof_file).unwrap();
+    let zk_genesis_file = std::fs::File::open("zkgenesis2.json").unwrap();
+    let header1_file = std::fs::File::open("nethead2.json").unwrap();
+    let header2_file = std::fs::File::open("skipheader.json").unwrap();
+    let zk_genesis: ExtendedHeader = serde_json::from_reader(zk_genesis_file).unwrap();
+    let nil_h1: Option<ExtendedHeader> = None;
+    let h1: Option<ExtendedHeader> = serde_json::from_reader(header1_file).unwrap();
     let h2: ExtendedHeader = serde_json::from_reader(header2_file).unwrap();
     
-    let encoded_1 = serde_cbor::to_vec(&h1).unwrap();
-    let encoded_2 = serde_cbor::to_vec(&h2).unwrap();
+    // Create first proof
+    let encoded_1 = serde_cbor::to_vec(&nil_h1).unwrap();
+    let encoded_2 = serde_cbor::to_vec(&zk_genesis).unwrap();
     
-    // write the headers
+    // write verification key
+    stdin.write(&vk.hash_u32());
+    // write the "public values" 
+    // (they're just some junk in the first iteration of the IVC)
+    stdin.write(&proof.public_values.to_vec());
+    // write genesis hash
+    stdin.write_vec(zk_genesis.header.hash().as_bytes().to_vec());
+    // write header1 (nil)
     stdin.write_vec(encoded_1);
+    // write header2 (genesis)
     stdin.write_vec(encoded_2);
+    // write the proof
+    stdin.write_proof(proof.proof, vk.vk);
 
-    let client = ProverClient::new();
-    let (pk, _vk) = client.setup(&ELF);
-
-
-    let start_time = Instant::now();
-    let mut proof = client.prove_compressed(&pk, stdin).expect("couldn't prove");
-    let end_time = Instant::now();
-    println!("Proving time: {:?}", end_time - start_time);
-    //let mut public_values = client.execute(&ELF, stdin).unwrap();
-    //let result = proof.public_values.to_vec();
-    let result: bool = proof.public_values.read();
-    let proof_file = std::fs::File::create("proof.json").unwrap();
-    let pk_file = std::fs::File::create("pk.json").unwrap();
-    serde_json::to_writer(proof_file, &proof).unwrap();
-
-    println!("result is: {:?}", result);
+    let mut public_values = client.execute(ELF, stdin).expect("could not execute");
+    println!("public values: {:?}", public_values);
+    let vkey_out: Vec<u8> = public_values.read();
+    let zk_genesis_hash: Vec<u8> = public_values.read();
+    let h2_hash_out: Vec<u8> = public_values.read();
+    let result: bool = public_values.read();
+    println!("vkey: {:?}", vkey_out);
+    println!("zk_genesis_hash: {:?}", zk_genesis_hash);
+    println!("h2_hash: {:?}", h2_hash_out);
+    println!("result: {:?}", result);
 }
